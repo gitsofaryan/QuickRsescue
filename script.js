76a08da12b249886b2e23406878a4f239da864ff
Aryan <117700812+gitsofaryan@users.noreply.github.com>
2023-09-23 00:32:29 +0530

    changes

--- script.js
+++ script.js
@@ -1,62 +1,15 @@
 ////map///////////////////////////////////////////
 
-let map, infoWindow;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
-
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        },
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation.",
-  );
-  infoWindow.open(map);
-}
-
-window.initMap = initMap;
 
 
 
 
 
 // table.js /////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 
 // Get a reference to the "Register" button
@@ -111,14 +64,7 @@ registerButton.addEventListener("click", function (event) {
 
 ////////////////////alert/////////////////////////////
 
-// const alert = document.querySelector("#alert");
 
-// alert.addEventListener("click", ()=>{
-//   alert.log("Alert sent");
-// })
-
-
-// Get references to the "Send alert" button and the modal elements
 const sendAlertButton = document.querySelectorAll(".send-alert-button");
 const alertModal = document.getElementById("alertModal");
 const closeAlertButton = document.getElementById("closeAlertButton");
@@ -143,4 +89,26 @@ sendAlertButton.forEach((button) => {
 // Add click event listener to the "Close" button in the modal
 closeAlertButton.addEventListener("click", () => {
     hideAlertModal();
+});
+
+
+// Add click event listener to the "Close" button in the modal
+closeAlertButton.addEventListener("click", () => {
+    hideAlertModal();
+});
+
+// Your existing code for the "Register" button and other functionality
+// ...
+
+
+
+const locationTd = document.querySelector("#locate");
+
+// Get a reference to the map element
+const mapDiv = document.querySelector("#map");
+
+// Add a click event listener to the location td
+locationTd.addEventListener("click", () => {
+    // Scroll the map element into view when the location td is clicked
+    mapDiv.scrollIntoView({ behavior: "smooth" });
 });
